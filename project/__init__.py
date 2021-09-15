95c8ac3457763ac0a348de5bd20e551d12e51d52
Shirley-April <shirleyapril23@gmail.com>
2021-09-15 21:51:50 +0300

    adding the db

--- project/__init__.py
+++ project/__init__.py
@@ -1,21 +1,22 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-
-
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = 'cdccefb9e9b1f59ecbb3d6e3653ac636'
-    app.config['SQLaLCHEMY_DATABASE_URI'] = "sqlite///site.db"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite"
     db.init_app(app)
 
     # from .models import User
     
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
+    
+    with app.app_context():
+        db.create_all()
 
     return app
 
